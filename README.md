fb174ae82ab44efcc4899046fef3527eac7e3834
William Dahlberg <dahlberg.william@gmail.com>
2015-11-30 13:43:54 +0100

    space.py initialized

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pong
+A simple game of Pong.
+Play by running pong.py.
+Made with Python 2.7 and pygame.
---
+++ spaceshooter/space.py
@@ -0,0 +1,13 @@
+import sys, os
+import pygame
+from pygame.locals import *
+
+
+class Ship(pygame.sprite.Sprite):
+	pass
+
+class Enemy(pygame.sprite.Sprite):
+	pass
+
+class Ammo(pygame.sprite.Sprite):
+	pass
