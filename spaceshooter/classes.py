eb4669d355f95fe5d7daa1e79bfb1d547bd51e7b
William Dahlberg <dahlberg.william@gmail.com>
2015-12-11 23:09:15 +0100

    Ship now moves and fires bullets!

--- spaceshooter/classes.py
+++ spaceshooter/classes.py
@@ -6,13 +6,23 @@ class Ship(pygame.sprite.Sprite):
 	def __init__(self):
 		# fix sprites and rects and stuff
 		self.health = 50
+		self.speed = 10
+		self.rect = pygame.Rect(300, 850, 15, 15)
+
+	def move(self, dir):
+		self.rect.x += self.speed * dir
 
 class Enemy(pygame.sprite.Sprite):
 	def __init__(self, health = 10):
 		# fixx sprites and rects and stuff
-
+		self.health = health
 
 class Bullet(pygame.sprite.Sprite):
 	def __init__(self, dmg = 5):
 		# fix sprites and rects and stuff
 		self.damage = dmg
+		self.speed = -6
+		self.rect = pygame.Rect(0, 0, 3, 3)
+
+	def move(self):
+		self.rect.y += self.speed
--- spaceshooter/space.py
+++ spaceshooter/space.py
@@ -5,11 +5,63 @@ from classes import *
 
 
 class Main:
+    def __init__(self):
+        pygame.init()
+        self.width, self.height = 640, 900
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        self.bullets = []
+        self.player = Ship()
+        self.fired = 0
 
-	def __init__(self):
-		pygame.init()
-		self.width, self.height = 640, 480
-		self.screen = pygame.display.set_mode(self.width, self.height)
+    def process_input(self):
+        keys = pygame.key.get_pressed()
 
-	def game(self):
-		
+        if keys[K_ESCAPE]:
+            sys.exit()
+        if keys[K_LEFT]:
+            self.player.move(LEFT)
+        if keys[K_RIGHT]:
+            self.player.move(RIGHT)
+        if keys[K_UP]:
+            self.fire()
+
+
+    def fire(self):
+        if self.fired > 0: 
+            return
+        bullet = Bullet()
+        bullet.rect.y = self.player.rect.y + 5
+        bullet.rect.x = self.player.rect.x + 2
+        self.bullets.append(bullet)
+        self.fired = 10
+
+    def update_bullets(self):
+        for b in self.bullets:
+            b.move()
+            # check for collisions, deal damage
+            pygame.draw.rect(self.screen, (255, 255, 255), b.rect)
+            if b.rect.bottom < 0:
+                self.bullets.remove(b)
+
+    def game(self):
+        clock = pygame.time.Clock()
+
+        while 1:
+            self.screen.fill((0, 0, 0))
+            clock.tick(60)
+            if self.fired > 0:
+                self.fired -= 1
+
+            for e in pygame.event.get():
+                if e.type == QUIT:
+                    sys.exit()
+
+            self.process_input()
+            self.update_bullets()
+
+            pygame.draw.rect(self.screen, (255, 255, 255), self.player.rect)
+            pygame.display.flip()
+RIGHT = 1
+LEFT = -1
+if __name__ == "__main__":
+    Main().game()
